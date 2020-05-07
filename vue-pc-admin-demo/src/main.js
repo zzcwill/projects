4e458e95b7de254956f4d1884872ba3e48c93332
zhengzhichao <377950622@qq.com>
2020-05-07 11:01:37 +0800

    删除冗余代码

--- vue-pc-admin-demo/src/main.js
+++ vue-pc-admin-demo/src/main.js
@@ -14,13 +14,9 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-import loadsh from 'loadsh'
-import moment from 'moment'
+// import loadsh from 'loadsh'
+// import moment from 'moment'
 
-import VueQuillEditor from 'vue-quill-editor' // 富文本编辑器
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
 
 // 引入全局过滤器
 import '@/filters'
@@ -38,13 +34,12 @@ Vue.use(ElementUI,
     size: 'small'
   }
 )
-Vue.use(VueQuillEditor)
 
 Vue.config.productionTip = false
 
 // 公共方法引入
-Vue.prototype._ = loadsh
-Vue.prototype.moment = moment
+// Vue.prototype._ = loadsh
+// Vue.prototype.moment = moment
 
 new Vue({
   el: '#app',
