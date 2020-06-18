822dc1a9d87837d1fd1901bd2c283494df5578ee
zhengzhichao <377950622@qq.com>
2020-06-18 17:39:05 +0800

    express-demo处理

--- node-express-demo/router/api.js
+++ node-express-demo/router/api.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-// router.get('/login', function(req, res, next) {
-//   res.send('login');
-// });
+var { resDataApi } = require('../extend/api');
 
-router.get('/login', function(req, res, next) {
-  res.send('login');
+console.info(resDataApi(20000,'','没有传way参数'))
+
+router.get('/get', function(req, res, next) {
+  var data = {};
+  if(!req.params.way) {
+    data = resDataApi(20000,'','没有传way参数');
+  }
+
+  if(req.params.way) {
+    data = resDataApi('',{
+      way: req.params.way
+    },'ok');
+  }
+
+  res.json(data)
 });
 
 router.get('/loginOut', function(req, res, next) {
