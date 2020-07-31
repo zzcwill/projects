8aef8d7afa78d590a25b45c6c4f50ccdc6536807
zhengzhichao <377950622@qq.com>
2020-07-31 16:02:35 +0800

    react-h5-demo模板增加

--- react-h5-demo/config-overrides.js
+++ react-h5-demo/config-overrides.js
@@ -10,9 +10,9 @@ module.exports = override(
 		['@']: resolve('src')
 	}),
   // 针对antd实现按需打包: 根据import来打包(使用babel-plugin-import)
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true, // 自动打包相关的样式
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css'
   }),
 )
