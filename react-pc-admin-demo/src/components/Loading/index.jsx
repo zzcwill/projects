0fe837d5f37c05b75b898288ad91d2afe511770b
zhengzhichao <377950622@qq.com>
2020-08-05 17:33:44 +0800

    代码优化

--- react-pc-admin-demo/src/components/Loading/index.jsx
+++ react-pc-admin-demo/src/components/Loading/index.jsx
@@ -1,18 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Spin } from "antd";
-import NProgress from "nprogress"; // progress bar
-import "nprogress/nprogress.css"; // progress bar style
-
-NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
 const Loading = () => {
-  useEffect(() => {
-    NProgress.start();
-    return () => {
-      NProgress.done();
-    };
-  }, []);
-
   return (
     <div className="app-container">
       <Spin />
