8dd56ed815947798fe862c12b7c3eec1ba0a7a64
zhengzhichao <377950622@qq.com>
2021-01-12 11:32:48 +0800

    删除多余项目

--- react-antd-pc-v4/src/layouts/BlankLayout.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Inspector } from 'react-dev-inspector';
-
-const InspectorWrapper = process.env.NODE_ENV === 'development' ? Inspector : React.Fragment;
-
-const Layout = ({ children }) => <InspectorWrapper>{children}</InspectorWrapper>;
-
-export default Layout;
