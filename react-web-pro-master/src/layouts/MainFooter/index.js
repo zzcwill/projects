d85e77655e842644bc23c354a953253e243f2d2f
zhengzhichao <377950622@qq.com>
2021-01-25 14:54:53 +0800

    删除冗余项目

--- react-web-pro-master/src/layouts/MainFooter/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Layout } from 'antd';
-
-const MainFooter = () => <Layout.Footer>底部</Layout.Footer>;
-
-export default MainFooter;
--- react-web-pro-master/src/pages/Welcome/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './style.less';
-
-const WelcomePage = () => <div className="page-welcome" />;
-
-export default WelcomePage;
