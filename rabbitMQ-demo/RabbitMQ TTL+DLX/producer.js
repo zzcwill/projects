e26fe609e47404cb0386e8152338ef7d906f72ce
zhengzhichao <377950622@qq.com>
2021-04-13 23:23:22 +0800

    rabbitMQ-demo添加

---
+++ rabbitMQ-demo/RabbitMQ TTL+DLX/producer.js
@@ -0,0 +1,28 @@
+const rabbitmq = require('./rabbitmq.js');
+
+/**
+ * 路由一个死信队列
+ * @param { Object } connnection 
+ */
+ async function producerDLX(connnection) {
+    const testExchange = 'testEx';
+    const testQueue = 'testQu';
+    const testExchangeDLX = 'testExDLX';
+    const testRoutingKeyDLX = 'testRoutingKeyDLX';
+    
+    const ch = await connnection.createChannel();
+    await ch.assertExchange(testExchange, 'direct', { durable: true });
+    const queueResult = await ch.assertQueue(testQueue, {
+        exclusive: false,
+        deadLetterExchange: testExchangeDLX,
+        deadLetterRoutingKey: testRoutingKeyDLX,
+    });
+    await ch.bindQueue(queueResult.queue, testExchange);
+    const msg = 'hello world!';
+    console.log('producer msg：', msg);
+    await ch.sendToQueue(queueResult.queue, new Buffer(msg), {
+        expiration: '10000'
+    });
+    
+    ch.close();
+}
