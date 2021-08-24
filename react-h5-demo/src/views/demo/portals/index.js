7319228a215afe3beff19b1c12970554c3a4ba2f
zhengzhichao <377950622@qq.com>
2021-08-24 10:12:25 +0800

    react-children-update

--- react-h5-demo/src/views/demo/portals/index.js
+++ react-h5-demo/src/views/demo/portals/index.js
@@ -54,12 +54,35 @@ const Modal2 = ({title, text, isOpen, onClose, children}) => {
   , document.body)
 }
 
+const Modal3 = ({title, text, isOpen, onClose, children}) => {
+  if (!isOpen) return null;
+  return ReactDOM.createPortal(
+    <div className="Popup" v-show="visible">
+      <div className="mask"></div>
+      <div className="content">
+        <div className="top">
+          <div className="title">{title}</div>
+          <div className="txt">{text}</div>
+        </div>
+        <div className="center">
+          {children}
+        </div>
+        <div className="bottom">
+          <button onClick={onClose}>确定</button>
+        </div>
+      </div>
+    </div>
+  , document.body)
+}
+
 const Portals = (props) => {
   const { userInfo, history } = props;
 
   const [open, setOpen] = useState(false)
 
   const [open2, setOpen2] = useState(false)
+
+  const [open3, setOpen3] = useState(false)
 
   useEffect(() => {
   })
@@ -90,7 +113,17 @@ const Portals = (props) => {
         isOpen={open2}
         onClose={() => setOpen2(false)}
         children={hocChildren}
-      />      
+      />  
+      <br />
+      <div onClick={() => setOpen3(true)}>Portals-脱离父组件3-传组件3</div>
+      <Modal3
+        title="tip-title3"
+        text="text-text3"
+        isOpen={open3}
+        onClose={() => setOpen3(false)}
+      >
+        <BaseLayout />
+      </Modal3>     
     </div>
   )
 };
