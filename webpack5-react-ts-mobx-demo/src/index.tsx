26382730e52b272890ea126d958afe71dd20d3bc
zhengzc <zhengzc@servyou.com.cn>
2023-04-19 14:00:39 +0800

    eslint config

--- webpack5-react-ts-mobx-demo/src/index.tsx
+++ webpack5-react-ts-mobx-demo/src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './views/app';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './views/app'
 
 const initApp = () => {
   console.log('NODE_ENV', process.env.NODE_ENV)
   console.log('BASE_ENV', process.env.BASE_ENV)
 
   const root = document.getElementById('root')
+
   if(root) {
     createRoot(root).render(<App />)
   }
