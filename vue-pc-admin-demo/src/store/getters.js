7524ce59b9a0b13dfdbc863f6b88912e5de27cf5
zhengzc <zhengzc@servyou.com.cn>
2024-04-24 17:06:10 +0800

    feat: delete vue

--- vue-pc-admin-demo/src/store/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  visitedViews: state => state.tagsView.visitedViews,
-  cachedViews: state => state.tagsView.cachedViews,
-  routes: state => state.permission.routes,
-  userInfo: state => state.user.userInfo,
-}
-export default getters
--- vue-pc-admin-demo/src/utils/validate.js
+++
@@ -1,4 +0,0 @@
-// 登录页-用户名验证
-export function validUsername(str) {
-  return str !== ''
-}
