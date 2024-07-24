b718acf2679b4929a7d73311e7eb4522cd6cdc5c
zhengzc <zhengzc@servyou.com.cn>
2024-07-24 14:07:48 +0800

    remove express-mysql

--- node-express-mysql-demo/controller/csurfApi.js
+++
@@ -1,9 +0,0 @@
-var { resDataApi } = require('../extend/api');
-
-module.exports = {
-	get: async function (req, res, next) {
-		res.json(resDataApi(10000,{
-			data: 'csurf'
-		},'csurf-test'))
-	}
-}
--- node-express-mysql-demo/dba/index.js
+++
@@ -1,3 +0,0 @@
-var User = require('./user');
-
-exports.User = User;
